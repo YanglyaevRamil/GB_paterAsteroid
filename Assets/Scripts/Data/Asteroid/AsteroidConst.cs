aed9c0d90bd9515b2f05f1390cda05439d319607
Ramil <jangrash@mail.ru>
2022-02-10 21:18:50 +0300

    fix

--- Assets/Scripts/Data/Asteroid/AsteroidConst.cs
+++ Assets/Scripts/Data/Asteroid/AsteroidConst.cs
@@ -1,5 +1,14 @@
 public static class AsteroidConst
 {
     public const float DURATION_OF_DEATH = 3.0f;
+
+    public const float START_DELAY_ASTEROID = 0f;
+    public const float SPAWN_INTERVAL_ASTEROID = 1f;
+    public const float MAX_ANGL_X_RESP_ASTEROID = -20.0f;
+    public const float MIN_ANGL_X_RESP_ASTEROID = 20.0f;
+    public const int NUM_LAVA_ASTEROID_IN_GRUP = 3;
+    public const int NUM_FIRE_ASTEROID_IN_GRUP = 2;
+    public const int NUM_ICE_ASTEROID_IN_GRUP = 1;
+    public const int NUM_ASTEROID_GRUP = 30;
 }
 
--- Assets/Scripts/Managers/AsteroidManager.cs
+++ Assets/Scripts/Managers/AsteroidManager.cs
@@ -3,15 +3,6 @@ using UnityEngine;
 
 public class AsteroidManager : MonoBehaviour
 {
-    private const float START_DELAY_ASTEROID = 0f;
-    private const float SPAWN_INTERVAL_ASTEROID = 1f;
-    private const float MAX_ANGL_X_RESP_ASTEROID = -20.0f;
-    private const float MIN_ANGL_X_RESP_ASTEROID = 20.0f;
-    private const int NUM_LAVA_ASTEROID_IN_GRUP = 3;
-    private const int NUM_FIRE_ASTEROID_IN_GRUP = 2;
-    private const int NUM_ICE_ASTEROID_IN_GRUP = 1;
-    private const int NUM_ASTEROID_GRUP = 30;
-
     public Action<AsteroidData> OnDeadAsteroid;
     public Transform target;
 
@@ -27,28 +18,28 @@ public class AsteroidManager : MonoBehaviour
         asteroidPool = new GameObjectPool(NameManager.POOL_CONTENT_ASTEROID);
         asteroidDataFactory = new AsteroidDataFactory(target);
 
-        for (int i = 0; i < NUM_ASTEROID_GRUP; i++)
+        for (int i = 0; i < AsteroidConst.NUM_ASTEROID_GRUP; i++)
         {
-            for (int j = 0; j < NUM_LAVA_ASTEROID_IN_GRUP; j++)
+            for (int j = 0; j < AsteroidConst.NUM_LAVA_ASTEROID_IN_GRUP; j++)
             {
                 var asteroidData = asteroidDataFactory.InstantiateAsteroid(AsteroidType.Lava);
 
                 DeploymentAsteroidData(asteroidData);
             }
-            for (int k = 0; k < NUM_FIRE_ASTEROID_IN_GRUP; k++)
+            for (int k = 0; k < AsteroidConst.NUM_FIRE_ASTEROID_IN_GRUP; k++)
             {
                 var asteroidData = asteroidDataFactory.InstantiateAsteroid(AsteroidType.Fire);
 
                 DeploymentAsteroidData(asteroidData);
             }
-            for (int l = 0; l < NUM_ICE_ASTEROID_IN_GRUP; l++)
+            for (int l = 0; l < AsteroidConst.NUM_ICE_ASTEROID_IN_GRUP; l++)
             {
                 var asteroidData = asteroidDataFactory.InstantiateAsteroid(AsteroidType.Ice);
 
                 DeploymentAsteroidData(asteroidData);
             }
         }
-        InvokeRepeating("GetAsteroidInPool", START_DELAY_ASTEROID, SPAWN_INTERVAL_ASTEROID);
+        InvokeRepeating("GetAsteroidInPool", AsteroidConst.START_DELAY_ASTEROID, AsteroidConst.SPAWN_INTERVAL_ASTEROID);
     }
 
     private void DeadAsteroid(AsteroidData asteroidData)
@@ -68,7 +59,7 @@ public class AsteroidManager : MonoBehaviour
 
     private void GetAsteroidInPool()
     {
-        float rndX = UnityEngine.Random.Range(MIN_ANGL_X_RESP_ASTEROID, MAX_ANGL_X_RESP_ASTEROID);
+        float rndX = UnityEngine.Random.Range(AsteroidConst.MIN_ANGL_X_RESP_ASTEROID, AsteroidConst.MAX_ANGL_X_RESP_ASTEROID);
         var asteroid = asteroidPool.GetObject();
         asteroid.transform.localPosition = target.TransformPoint(rndX, 0 , 300);
         asteroid.SetActive(true);
